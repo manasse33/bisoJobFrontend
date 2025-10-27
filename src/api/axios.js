31821a8ca99dd2e71f7432d24fd636cb2d7afdd5
manasse33 <143910765+manasse33@users.noreply.github.com>
2025-10-27 23:17:30 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.147:8000/api/v1',
+  baseURL: 'https://bisojob.gt.tc/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
